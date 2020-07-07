e25b282ff54c4c39625c07c9b8b9be6c04f108c6
deepzS2 <alanbackman10@hotmail.com>
2020-07-07 12:21:17 -0300

    refactor: 💡 Functions.ts

---
+++ src/functions.ts
@@ -0,0 +1,27 @@
+import { Message, GuildMember } from 'discord.js'
+
+export default {
+  getMember: function (msg: Message, toFind = ''): GuildMember {
+    toFind = toFind.toLowerCase()
+
+    let target = msg.guild.member(toFind)
+
+    if (!target && msg.mentions.members) target = msg.mentions.members.first()
+
+    if (!target && toFind) {
+      target = msg.guild.members.cache.find((member) => {
+        return (
+          member.displayName.toLowerCase().includes(toFind) ||
+          member.user.tag.toLowerCase().includes(toFind)
+        )
+      })
+    }
+
+    if (!target) target = msg.member
+
+    return target
+  },
+  formatDate: function (date: Date): string {
+    return new Intl.DateTimeFormat('pt-BR').format(date)
+  },
+}

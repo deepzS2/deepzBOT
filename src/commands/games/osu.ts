21e96b32aa7d0cc7de51ed66170bfb3885fa7c4e
deepzS2 <alanbackman10@hotmail.com>
2020-07-21 15:50:46 -0300

    feat: 🎸 Osu utils, new pp calculator
    Moved the functions to utils file for future TODOs and using ojsama to
    calculate PP.

--- src/commands/games/osu.ts
+++ src/commands/games/osu.ts
@@ -1,15 +1,12 @@
 import { stripIndents } from 'common-tags'
 import { MessageEmbed } from 'discord.js'
 import dotenv from 'dotenv'
-import JSSoup from 'jssoup'
-import { Api, Score, User } from 'node-osu'
-import request from 'request-promise-native'
+import { Api, Score } from 'node-osu'
 
 import { Command } from '@customTypes/commands'
 import connection from '@database'
 import { CommandContext } from '@models/command_context'
-
-const osuBpdpc = require('osu-bpdpc')
+import { getScore } from '@utils/osu'
 
 // .env
 dotenv.config()
@@ -43,13 +40,13 @@ export default class OsuCommand implements Command {
 
   commandCategory = 'Games'
 
-  commandUsage = 'd.osu'
+  commandUsage = 'd.osu [recentscore | set] [username(set)]'
 
   getHelpMessage(commandPrefix: string): string {
     return `Use ${commandPrefix}osu to set your osu! settings, get recent plays\nNOTE: Only support standard and under development...`
   }
 
-  async run({ originalMessage, args }: CommandContext): Promise<void> {
+  async run({ originalMessage, args, bot }: CommandContext): Promise<void> {
     if (!args[0]) {
       try {
         const user = await connection('users')
@@ -95,14 +92,88 @@ export default class OsuCommand implements Command {
 
         const { osu } = user
 
-        const message = await getRecent(osu)
+        const [recent] = await osuApi.getUserRecent({ u: osu })
+        const osuUser = await osuApi.getUser({ u: recent.user.id })
 
-        if (message instanceof Error && message.message === 'Not found') {
-          await originalMessage.channel.send(`**:x: No recent plays**`)
-          return
-        }
+        const recent_raw = formatRecentRaw(recent)
 
-        await originalMessage.channel.send(message)
+        await getScore(recent_raw, async (err, score) => {
+          if (err) {
+            await originalMessage.channel.send(`**:x: ${err.message}**`)
+            return
+          }
+
+          let description = ''
+
+          const message = new MessageEmbed()
+            .setAuthor(
+              `${osuUser.name} - ${
+                osuUser.pp.raw
+              }pp (#${osuUser.pp.rank.toLocaleString()})`,
+              `https://s.ppy.sh/a/${score.user_id}`,
+              `https://osu.ppy.sh/u/${recent.user.id}`
+            )
+            .setTitle(`${score.artist} - ${score.title} [${score.version}]`)
+            .setURL(`https://osu.ppy.sh/b/${score.beatmap_id}`)
+            .setThumbnail(
+              `https://b.ppy.sh/thumb/${recent.beatmap.beatmapSetId}l.jpg`
+            )
+
+          const emoji = bot.emojis.cache.find(
+            (value) => value.name === `${recent.rank}_`
+          )
+
+          description += `➤ **${emoji} rank** `
+
+          if (score.pp_fc > score.pp) {
+            description += `➤ **${+score.pp.toFixed(
+              2
+            )}pp (${+score.pp_fc.toFixed(2)}pp for ${+score.acc_fc.toFixed(
+              2
+            )}% FC)**\n➤ **Accuracy:** ${score.acc.toFixed(2)}%`
+          } else {
+            description += `➤ **${+score.pp.toFixed(2)}pp**`
+          }
+
+          if (score.combo < score.max_combo) {
+            description += `\n➤ **${recent.score.toLocaleString()}** ➤ **${
+              score.combo
+            }/${score.max_combo}x**`
+          } else {
+            description += `**${score.max_combo}x**`
+          }
+
+          if (score.count300 > 0) {
+            description += ` ➤ [${score.count300}, `
+          }
+
+          if (score.count100 > 0) {
+            description += `${score.count100}, `
+          }
+
+          if (score.count50 > 0) {
+            description += `${score.count50}, `
+          }
+
+          if (score.countmiss > 0) {
+            description += `${score.countmiss}]`
+          }
+
+          console.log(score.map_completion)
+
+          if (score.map_completion) {
+            description += `\n➤ **Map completion:** ${score.map_completion.toFixed(
+              2
+            )}%`
+          }
+
+          message.setDescription(description)
+
+          originalMessage.channel.send(
+            `**Most recent play for ${osuUser.name}**`,
+            message
+          )
+        })
       } catch (error) {
         await originalMessage.channel.send(
           `**:x: Something went wrong! Please try again later.**`
@@ -163,6 +234,24 @@ export default class OsuCommand implements Command {
   }
 }
 
+function formatRecentRaw(recent: Score) {
+  return {
+    user_id: recent.user.id,
+    beatmap_id: recent.beatmap.id,
+    rank: recent.rank,
+    score: recent.score,
+    combo: recent.maxCombo,
+    count300: recent.counts['300'],
+    count100: recent.counts['100'],
+    count50: recent.counts['50'],
+    countmiss: recent.counts.miss,
+    modsNumber: recent.raw_mods,
+    modsString: recent.mods,
+    date: new Date(recent.date),
+    pp: recent.pp,
+  }
+}
+
 async function getUserEmbed(name: string): Promise<MessageEmbed> {
   try {
     const player = await osuApi.getUser({ u: name, type: 'string' })
@@ -209,267 +298,4 @@ async function getUserEmbed(name: string): Promise<MessageEmbed> {
 
     return error
   }
-}
-
-async function getRecent(name: string): Promise<MessageEmbed | Error> {
-  try {
-    const recents = await osuApi.getUserRecent({
-      u: name,
-      limit: 1,
-      type: 'string',
-    })
-
-    if (recents.length === 0) {
-      return new Error('User has no recent score...')
-    }
-
-    const recent = recents[0]
-
-    let mods: string | Array<string> = getMods(recent.raw_mods)
-
-    if (mods.length === 0) {
-      mods = []
-      mods.push('No Mod')
-    } else {
-      const oppai_mods = `+${mods.join(' ')}`
-      mods = oppai_mods
-    }
-
-    const { acc, fc } = await calculateAccAndFc(recent)
-    const player = await getUser(name)
-    const beatmap_url = `https://osu.ppy.sh/b/${recent.beatmapId}`
-
-    const map_image = await getBeatmapThumb(beatmap_url)
-
-    let pot_pp = ''
-    let mapCompletion
-    if (recent.rank === 'F') {
-      const total_hits =
-        recent.counts['50'] +
-        recent.counts['100'] +
-        recent.counts['300'] +
-        recent.counts.miss
-
-      const output = await getPP(
-        recent.beatmapId as string,
-        recent.raw_mods,
-        recent.maxCombo,
-        recent.beatmap.maxCombo,
-        recent.counts,
-        [acc],
-        total_hits
-      )
-
-      if (output) {
-        pot_pp = `**No PP** (${output.pp}PP for ${fc.toFixed(2)}% FC)`
-        mapCompletion = output.mapCompletion
-      }
-    } else {
-      const output = await getPP(
-        recent.beatmapId as string,
-        recent.raw_mods,
-        recent.maxCombo,
-        recent.beatmap.maxCombo,
-        recent.counts,
-        [acc]
-      )
-
-      if (output) {
-        pot_pp = `**${output.pp}PP**`
-        mapCompletion = output.mapCompletion
-      }
-    }
-
-    let desc = `**${
-      recent.beatmap.title
-    } [${recent.beatmap.difficulty.rating.toFixed(2)}✩] +${mods}**\n`
-
-    desc += `-> **${
-      recent.rank
-    } Rank** -> ${pot_pp}\n-> **Accuracy:** ${acc.toFixed(2)}%\n`
-
-    desc += `-> **${recent.score}** -> x${recent.maxCombo}/${recent.beatmap.maxCombo} -> [${recent.counts['300']}/${recent.counts['100']}/${recent.counts['50']}/${recent.counts.miss}]\n`
-
-    if (recent.rank === 'F') {
-      desc += `-> **Map Completion:** ${mapCompletion.toFixed(2)}%`
-    }
-
-    const embed = new MessageEmbed()
-      .setTitle(`**${player.name} most recent play**`)
-      .setDescription(desc)
-      .setAuthor(name, `http://s.ppy.sh/a/${player.id}`)
-      .setThumbnail(map_image.replace(/\\/g, ''))
-      .setColor('#4360FB')
-
-    return embed
-  } catch (error) {
-    console.error(error)
-
-    return error
-  }
-}
-
-function getMods(number: number): Array<string> {
-  const mod_list = []
-
-  if (number & (1 << 0)) mod_list.push('NF')
-  if (number & (1 << 1)) mod_list.push('EZ')
-  if (number & (1 << 3)) mod_list.push('HD')
-  if (number & (1 << 4)) mod_list.push('HR')
-  if (number & (1 << 5)) mod_list.push('SD')
-  if (number & (1 << 9)) mod_list.push('NC')
-  else if (number & (1 << 6)) mod_list.push('DT')
-  if (number & (1 << 7)) mod_list.push('RX')
-  if (number & (1 << 8)) mod_list.push('HT')
-  if (number & (1 << 10)) mod_list.push('FL')
-  if (number & (1 << 12)) mod_list.push('SO')
-  if (number & (1 << 14)) mod_list.push('PF')
-  if (number & (1 << 15)) mod_list.push('4 KEY')
-  if (number & (1 << 16)) mod_list.push('5 KEY')
-  if (number & (1 << 17)) mod_list.push('6 KEY')
-  if (number & (1 << 18)) mod_list.push('7 KEY')
-  if (number & (1 << 19)) mod_list.push('8 KEY')
-  if (number & (1 << 20)) mod_list.push('FI')
-  if (number & (1 << 24)) mod_list.push('9 KEY')
-  if (number & (1 << 25)) mod_list.push('10 KEY')
-  if (number & (1 << 26)) mod_list.push('1 KEY')
-  if (number & (1 << 27)) mod_list.push('3 KEY')
-  if (number & (1 << 28)) mod_list.push('2 KEY')
-
-  return mod_list
-}
-
-function calculateAccAndFc(score: Score): Record<string, number> {
-  let total_unscale_score = score.counts[300]
-  total_unscale_score += score.counts[100]
-  total_unscale_score += score.counts[50]
-  total_unscale_score += score.counts.miss
-  total_unscale_score *= 300
-
-  let user_score_acc = score.counts[300] * 300
-  user_score_acc += score.counts[100] * 100
-  user_score_acc += score.counts[50] * 50
-
-  const acc = (user_score_acc / total_unscale_score) * 100
-
-  let user_no_choke_acc = score.counts[300] * 300
-  user_no_choke_acc += (score.counts[100] + score.counts.miss) * 100
-  user_no_choke_acc += score.counts[50] * 50
-
-  const fc = (user_no_choke_acc / total_unscale_score) * 100
-
-  return { acc, fc }
-}
-
-async function getUser(name: string): Promise<User> {
-  try {
-    const user = await osuApi.getUser({
-      u: name,
-    })
-
-    return user
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-async function getPP(
-  map_id: string,
-  mods = 0,
-  combo: undefined | number = undefined,
-  max_combo: number,
-  counts: Record<string, number>,
-  accs = [100],
-  completion: number | undefined = undefined
-): Promise<Record<string, unknown>> {
-  try {
-    const osu = await request.get(`https://osu.ppy.sh/osu/${map_id}`)
-
-    const beatmap = osuBpdpc.Beatmap.fromOsu(osu)
-
-    const score = {
-      maxcombo: combo || 0,
-      count50: counts['50'],
-      count100: counts['100'],
-      count300: counts['300'],
-      countMiss: counts.miss,
-      countKatu: counts.katu,
-      countGeki: counts.geki,
-      perfect: counts.perfect ? 1 : 0,
-      mods,
-    }
-
-    const diff = osuBpdpc.Osu.DifficultyCalculator.use(beatmap)
-      .setMods(mods)
-      .calculate()
-
-    const perf = osuBpdpc.Osu.PerformanceCalculator.use(diff).calculate(score)
-
-    const pp_json = {
-      version: perf.beatmap.Version,
-      title: perf.beatmap.Metadata.Title,
-      artist: perf.beatmap.Metadata.Artist,
-      creator: perf.beatmap.Metadata.Creator,
-      combo,
-      misses: counts.miss,
-      max_combo,
-      hitObjects: diff.beatmap.HitObjects.length,
-      hitCircles: diff.beatmap.HitObjects.filter(
-        (value) => value.constructor.name === 'Circle'
-      ),
-      hitSliders: diff.beatmap.HitObjects.filter(
-        (value) => value.constructor.name === 'Slider'
-      ),
-      hitSpinners: diff.beatmap.HitObjects.filter(
-        (value) => value.constructor.name === 'Spinner'
-      ),
-      stars: perf.starDifficulty.toFixed('2'),
-      aimStars: perf.aimDifficulty.toFixed('2'),
-      speedDifficulty: perf.speedDifficulty.toFixed('2'),
-      pp: perf.totalPerformance.toFixed('2'),
-      aimPp: perf.aimPerformance.toFixed('2'),
-      speedPp: perf.speedPerformance.toFixed('2'),
-      accPp: perf.accuracyPerformance.toFixed('2'),
-      accs,
-      cs: perf.beatmap.Difficulty.CircleSize,
-      hp: perf.beatmap.Difficulty.HPDrainRate,
-      ar: perf.beatmap.Difficulty.ApproachRate,
-      od: perf.beatmap.Difficulty.OverallDifficulty,
-      mapCompletion: undefined,
-    }
-
-    if (completion) {
-      const hitObj = []
-
-      const numObj = completion - 1
-      const num = diff.beatmap.HitObjects.length
-
-      diff.beatmap.HitObjects.forEach((value) => {
-        hitObj.push({ start: value.startTime, end: value.endTime })
-      })
-
-      const timing = hitObj[num - 1].end - hitObj[0].start
-      const point = hitObj[numObj].end - hitObj[0].start
-
-      pp_json.mapCompletion = (point / timing) * 100
-    }
-
-    return pp_json
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-async function getBeatmapThumb(url: string): Promise<string> {
-  const res = await request.get(url)
-  const soup = new JSSoup(res)
-
-  const scripts = soup.findAll('script')
-  const script = (scripts[scripts.length - 5].text as string).split(
-    '"list@2x":"'
-  )
-
-  const index = script[1].indexOf('"')
-
-  return script[1].slice(0, index)
 }
---
+++ src/utils/osu.ts
@@ -0,0 +1,428 @@
+/* eslint-disable new-cap */
+import request from 'request-promise-native'
+
+// eslint-disable-next-line import-helpers/order-imports
+import { parser } from 'ojsama'
+
+// require because one declaration file is wrong on the module!
+const osu = require('ojsama')
+
+const ar_ms_step1 = 120
+const ar_ms_step2 = 150
+
+const ar0_ms = 1800
+const ar5_ms = 1200
+const ar10_ms = 450
+
+const od_ms_step = 6
+const od0_ms = 79.5
+const od10_ms = 19.5
+
+const DIFF_MODS = ['HR', 'EZ', 'DT', 'HT']
+
+const mods_enum = {
+  '': 0,
+  NF: 1,
+  EZ: 2,
+  TD: 4,
+  HD: 8,
+  HR: 16,
+  SD: 32,
+  DT: 64,
+  RX: 128,
+  HT: 256,
+  NC: 512,
+  FL: 1024,
+  AT: 2048,
+  SO: 4096,
+  AP: 8192,
+  PF: 16384,
+  '4K': 32768,
+  '5K': 65536,
+  '6K': 131072,
+  '7K': 262144,
+  '8K': 524288,
+  FI: 1048576,
+  RD: 2097152,
+  LM: 4194304,
+  '9K': 16777216,
+  '10K': 33554432,
+  '1K': 67108864,
+  '3K': 134217728,
+  '2K': 268435456,
+  V2: 536870912,
+}
+
+interface RecentRaw {
+  user_id: string
+  beatmap_id: string
+  rank: string
+  score: number
+  combo: number
+  count300: number
+  count100: number
+  count50: number
+  countmiss: number
+  modsNumber: number
+  modsString: string[] | string
+  date: Date
+  pp: number
+}
+
+interface Recent {
+  user_id: string
+  beatmap_id: string
+  rank: string
+  combo: number
+  count300: number
+  count100: number
+  count50: number
+  countmiss: number
+  mods: string[] | string
+  date: Date
+  pp: number
+  approved: number
+  beatmapset_id: number
+  artist: string
+  title: string
+  version: string
+  bpm_min: number
+  bpm_max: number
+  max_combo: number
+  bpm: number
+  creator: string
+  creator_id: number
+  approved_date: Date
+  cs: number
+  ar: number
+  od: number
+  hp: number
+  duration: number
+  fail_percent: number
+  stars: number
+  pp_fc: number
+  acc: number
+  acc_fc: number
+  map_completion: number
+}
+
+interface Callback {
+  (error: Error | null, score?: Recent | undefined)
+}
+
+export async function getScore(
+  recent_raw: RecentRaw,
+  cb: Callback
+): Promise<void> {
+  let recent: Recent
+
+  recent = Object.assign(
+    {
+      user_id: recent_raw.user_id,
+      beatmap_id: recent_raw.beatmap_id,
+      rank: recent_raw.rank,
+      combo: recent_raw.combo,
+      count300: recent_raw.count300,
+      count100: recent_raw.count100,
+      count50: recent_raw.count50,
+      countmiss: recent_raw.countmiss,
+      mods: recent_raw.modsNumber,
+      date: recent_raw.date,
+    },
+    recent
+  )
+
+  if ('pp' in recent_raw && Number(recent_raw.pp) > 0) {
+    recent = Object.assign(
+      {
+        pp: recent_raw.pp,
+      },
+      recent
+    )
+  }
+
+  request
+    .get(`https://osu.lea.moe/b/${recent_raw.beatmap_id}`, { json: true })
+    .then(async (res) => {
+      const beatmap = res.beatmap
+
+      const diff_settings = calculateCsArOdHp(
+        beatmap.cs,
+        beatmap.ar,
+        beatmap.od,
+        beatmap.hp,
+        recent_raw.modsString
+      )
+
+      let speed = 1
+
+      if (recent_raw.modsString.includes('DT')) speed *= 1.5
+      else if (recent_raw.modsString.includes('HT')) speed *= 0.75
+
+      let fail_percent = 1
+
+      if (recent_raw.rank === 'F')
+        fail_percent =
+          (recent.count300 +
+            recent.count100 +
+            recent.count50 +
+            recent.countmiss) /
+          beatmap.hit_objects
+
+      recent = Object.assign(
+        {
+          approved: beatmap.approved,
+          beatmapset_id: beatmap.beatmapset_id,
+          artist: beatmap.artist,
+          title: beatmap.title,
+          version: beatmap.version,
+          bpm_min: beatmap.bpm_min * speed,
+          bpm_max: beatmap.bpm_max * speed,
+          max_combo: beatmap.max_combo,
+          bpm: beatmap.bpm * speed,
+          creator: beatmap.creator,
+          creator_id: beatmap.creator_id,
+          approved_date: beatmap.approved_date,
+          cs: diff_settings.cs,
+          ar: diff_settings.ar,
+          od: diff_settings.od,
+          hp: diff_settings.hp,
+          duration: beatmap.total_length,
+          fail_percent: fail_percent,
+        },
+        recent
+      )
+
+      const diff =
+        res.difficulty[
+          getModsEnum(
+            (recent_raw.modsString as string[]).filter((mod) =>
+              DIFF_MODS.includes(mod)
+            )
+          )
+        ]
+
+      if (diff.aim && diff.speed) {
+        let map_completion = 0
+
+        try {
+          const beatmapData = await getBeatmap(recent_raw.beatmap_id)
+          const parser = new osu.parser().feed(beatmapData)
+
+          const totalhits =
+            recent_raw.count300 +
+            recent_raw.count100 +
+            recent_raw.count50 +
+            recent_raw.countmiss
+
+          map_completion = await mapCompletion(parser, totalhits)
+        } catch (error) {
+          console.error(error)
+          cb(
+            new Error(
+              'Something went wrong while loading the map! Please try again later!'
+            )
+          )
+          return
+        }
+
+        const pp = osu.ppv2({
+          aim_stars: diff.aim,
+          speed_stars: diff.speed,
+          base_ar: beatmap.ar,
+          base_od: beatmap.od,
+          n100: recent_raw.count100,
+          n50: recent_raw.count50,
+          mods: recent_raw.modsNumber,
+          combo: recent_raw.combo,
+          ncircles: beatmap.num_circles,
+          nsliders: beatmap.num_sliders,
+          nobjects: beatmap.hit_objects,
+          max_combo: beatmap.max_combo,
+          nmiss: recent_raw.countmiss,
+        })
+
+        const pp_fc = osu.ppv2({
+          aim_stars: diff.aim,
+          speed_stars: diff.speed,
+          base_ar: beatmap.ar,
+          base_od: beatmap.od,
+          n100: recent_raw.count100,
+          n50: recent_raw.count50,
+          mods: recent_raw.modsNumber,
+          ncircles: beatmap.num_circles,
+          nsliders: beatmap.num_sliders,
+          nobjects: beatmap.hit_objects,
+          max_combo: beatmap.max_combo,
+        })
+
+        recent = Object.assign(
+          {
+            stars: diff.total,
+            pp_fc: pp_fc.total,
+            acc: pp.computed_accuracy.value() * 100,
+            acc_fc: pp_fc.computed_accuracy.value() * 100,
+            map_completion,
+          },
+          recent
+        )
+
+        if (!('pp' in recent)) {
+          ;(recent as Recent).pp = pp.total
+        }
+      } else {
+        cb(
+          new Error('No difficulty data for this map! Please try again later.')
+        )
+
+        return
+      }
+
+      cb(null, recent)
+    })
+    .catch((err) => {
+      console.log(err)
+
+      cb(
+        new Error(
+          "Map not in the database, maps that are too new don't work yet"
+        )
+      )
+    })
+}
+
+function calculateCsArOdHp(
+  cs_raw: number,
+  ar_raw: number,
+  od_raw: number,
+  hp_raw: number,
+  mods_enabled: string[] | string
+): Record<string, number> {
+  let speed = 1
+  let ar_multiplier = 1
+  let ar
+  let ar_ms
+
+  if (mods_enabled.includes('DT')) {
+    speed *= 1.5
+  } else if (mods_enabled.includes('HT')) {
+    speed *= 0.75
+  }
+
+  if (mods_enabled.includes('HR')) {
+    ar_multiplier *= 1.4
+  } else if (mods_enabled.includes('EZ')) {
+    ar_multiplier *= 0.5
+  }
+
+  ar = ar_raw * ar_multiplier
+
+  if (ar <= 5) ar_ms = ar0_ms - ar_ms_step1 * ar
+  else ar_ms = ar5_ms - ar_ms_step2 * (ar - 5)
+
+  if (ar_ms < ar10_ms) ar_ms = ar10_ms
+  if (ar_ms > ar0_ms) ar_ms = ar0_ms
+
+  ar_ms /= speed
+
+  if (ar <= 5) ar = (ar0_ms - ar_ms) / ar_ms_step1
+  else ar = 5 + (ar5_ms - ar_ms) / ar_ms_step2
+
+  let cs
+  let cs_multiplier = 1
+
+  if (mods_enabled.includes('HR')) {
+    cs_multiplier *= 1.3
+  } else if (mods_enabled.includes('EZ')) {
+    cs_multiplier *= 0.5
+  }
+
+  cs = cs_raw * cs_multiplier
+
+  if (cs > 10) cs = 10
+
+  let od
+  let odms
+  let od_multiplier = 1
+
+  if (mods_enabled.includes('HR')) {
+    od_multiplier *= 1.4
+  } else if (mods_enabled.includes('EZ')) {
+    od_multiplier *= 0.5
+  }
+
+  od = od_raw * od_multiplier
+  odms = od0_ms - Math.ceil(od_ms_step * od)
+  odms = Math.min(od0_ms, Math.max(od10_ms, odms))
+
+  odms /= speed
+
+  od = (od0_ms - odms) / od_ms_step
+
+  let hp
+  let hp_multiplier = 1
+
+  if (mods_enabled.includes('HR')) {
+    hp_multiplier *= 1.4
+  } else if (mods_enabled.includes('EZ')) {
+    hp_multiplier *= 0.5
+  }
+
+  hp = hp_raw * hp_multiplier
+
+  if (hp > 10) hp = 10
+
+  return {
+    cs: cs,
+    ar: ar,
+    od: od,
+    hp: hp,
+  }
+}
+
+function getModsEnum(mods) {
+  let return_value = 0
+  mods.forEach((mod) => {
+    return_value |= mods_enum[mod.toUpperCase()]
+  })
+  return return_value
+}
+
+async function mapCompletion(beatmap: parser, totalhits = 0) {
+  try {
+    const hitobj = []
+
+    if (totalhits === 0) {
+      totalhits = beatmap.map.objects.length
+    }
+
+    const numobj = totalhits - 1
+    const num = beatmap.map.objects.length
+
+    beatmap.map.objects.forEach((value) => {
+      hitobj.push(value.time)
+    })
+
+    const timing = hitobj[num - 1] - hitobj[0]
+    const point = hitobj[numobj] - hitobj[0]
+
+    const map_completion = (point / timing) * 100
+
+    return map_completion
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function getBeatmap(beatmap_id: string) {
+  return new Promise((resolve, reject) => {
+    request
+      .get(`https://osu.ppy.sh/osu/${beatmap_id}`)
+      .then((res) => {
+        resolve(res)
+      })
+      .catch((err) => {
+        reject(err)
+      })
+  })
+}

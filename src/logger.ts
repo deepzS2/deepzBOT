1282a5f5a8cdb0e27aecb136ca4940b02afa9a06
deepzS2 <alanbackman10@hotmail.com>
2022-07-21 08:51:39 -0300

    🔊: Adding logs configuration

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,29 @@
+import { createWriteStream } from 'fs'
+import path from 'path'
 import pino from 'pino'
 
-import { isDev } from './config'
+const logFileDestination = path.join(
+  __dirname,
+  '..',
+  'logs',
+  `${new Date().format('MM-DD-YYYY')}.log`
+)
 
-export default pino({
-  enabled: isDev,
-})
+export default pino(
+  {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        levelFirst: true,
+        translateTime: 'dd-mm-yyyy, h:MM:ss TT',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+  pino.multistream([
+    {
+      stream: createWriteStream(logFileDestination, { flags: 'w' }),
+    },
+  ])
+)

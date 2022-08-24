ac2fc545b9ff6567000c9277e4468335dacabcb5
deepzS2 <alanbackman10@hotmail.com>
2022-08-24 11:57:58 -0300

    ✨: Funny commands

---
+++ src/commands/funny/hug.ts
@@ -0,0 +1,47 @@
+import { ApplicationCommandOptionType } from 'discord.js'
+
+import logger from '@deepz/logger'
+import { getMentions, isInteraction } from '@helpers'
+import { tenor } from '@services'
+import { Command, CustomMessageEmbed } from '@structures'
+
+export default new Command({
+  name: 'hug',
+  description: 'Hug a user!',
+  category: 'FUNNY',
+  slash: 'both',
+  options: [
+    {
+      name: 'user',
+      description: 'User to hug',
+      type: ApplicationCommandOptionType.User,
+      required: true,
+    },
+  ],
+  examples: ['d.kiss @user'],
+  run: async ({ interaction, args, message }) => {
+    const user = isInteraction(args)
+      ? args.getUser('user')
+      : getMentions(message).first()
+
+    try {
+      const result = await tenor.search({
+        query: 'hug anime',
+        type: 'gif',
+      })
+
+      const gifs = result.results
+      const toSend = Math.floor(Math.random() * (gifs.length - 1) + 1)
+
+      return new CustomMessageEmbed(' ', {
+        image: gifs[toSend].media[0].gif.url,
+        color: '#4360FB',
+        description: `***${
+          interaction?.user.username || message?.author.id
+        }, you hugged ${user.username} :smiling_face_with_3_hearts:***`,
+      })
+    } catch (error) {
+      logger.error(error)
+    }
+  },
+})
---
+++ src/commands/funny/love.ts
@@ -0,0 +1,49 @@
+import { ApplicationCommandOptionType } from 'discord.js'
+
+import logger from '@deepz/logger'
+import { getMentions, isInteraction } from '@helpers'
+import { Command, CustomMessageEmbed } from '@structures'
+
+export default new Command({
+  name: 'love',
+  description: 'Calculate the love affinity you have for another person!',
+  category: 'FUNNY',
+  slash: 'both',
+  options: [
+    {
+      name: 'user',
+      description: 'User to calculate affinity',
+      type: ApplicationCommandOptionType.User,
+      required: true,
+    },
+  ],
+  examples: ['d.love @user'],
+  run: async ({ interaction, args, message }) => {
+    const user = isInteraction(args)
+      ? args.getUser('user')
+      : getMentions(message).first()
+
+    try {
+      const love = Math.random() * 100
+
+      const loveIndex = Math.floor(love / 10)
+      const loveLevel = '💖'.repeat(loveIndex) + '💔'.repeat(10 - loveIndex)
+
+      return new CustomMessageEmbed(' ', {
+        color: '#4360FB',
+        thumbnail: user.displayAvatarURL(),
+        fields: [
+          {
+            name: `☁ **<@${user.id}>** loves **<@${
+              (interaction?.user ?? message.author).id
+            }>** this much:`,
+            value: `💟 ${Math.floor(love)}%\n\n${loveLevel}`,
+            inline: false,
+          },
+        ],
+      })
+    } catch (error) {
+      logger.error(error)
+    }
+  },
+})

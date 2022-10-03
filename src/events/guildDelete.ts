1643717f9a39d3e21fefeb4d5a0f175e2a7f216e
deepzS2 <alanbackman10@hotmail.com>
2022-10-03 11:01:36 -0300

    🥅: Guild catch errors

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -1,9 +1,22 @@
+import logger from '@deepz/logger'
 import { Event } from '@deepz/structures'
 
 export default new Event('guildCreate', async (client, guild) => {
-  await client.database.guild.delete({
-    where: {
-      discordId: guild.id,
-    },
-  })
+  try {
+    const guildExists = await client.database.guild.findFirst({
+      where: {
+        discordId: guild.id,
+      },
+    })
+
+    if (!guildExists) return
+
+    await client.database.guild.delete({
+      where: {
+        discordId: guild.id,
+      },
+    })
+  } catch (error) {
+    logger.error(error, 'Error deleting guild from database!')
+  }
 })
